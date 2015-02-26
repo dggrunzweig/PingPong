58c5ec17164c74c05cb8a001dd13bb953f57e454
dggruzweig <davidg91@stanford.edu>
2015-02-25 19:22:01 -0800

    Initial Commit

---
+++ Synergy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Synergy
+//
+//  Created by David Grunzweig on 2/25/15.
+//  Copyright (c) 2015 David Grunzweig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
